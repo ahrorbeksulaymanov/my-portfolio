13cdb6902347c7008ab1dcaacabd99d67c798514
ahrorbeksulaymanov <78092308+ahrorbeksulaymanov@users.noreply.github.com>
2023-01-04 10:27:37 +0500

    new projects are added

--- src/components/Portfolio/Portfolio.tsx
+++ src/components/Portfolio/Portfolio.tsx
@@ -2,11 +2,14 @@ import { Container } from "./styles";
 import ScrollAnimation from "react-animate-on-scroll";
 import intalim from '../../assets/intalim.png'
 import bianchi from '../../assets/bianchi.png'
+import arabiya from '../../assets/al-arabiya.png'
 import tian from '../../assets/tian.png'
 import infotian from '../../assets/info-tian.png'
 import process from '../../assets/process.png'
 import reseption from '../../assets/reseption.png'
 import shop from '../../assets/shop.png'
+import yoshlar from '../../assets/yoshlar.png'
+import clinic from '../../assets/clinic.png'
 
 export function Portfolio() {
   return (
@@ -41,22 +44,39 @@ export function Portfolio() {
           </a>
         </ScrollAnimation>
 
-        {/* <ScrollAnimation animateIn="flipInX">
-          <a target='_blank' href='https://clinic.proacademy.uz/'>
-          <div className="project">
-            <header>
-              <svg width="50" xmlns="http://www.w3.org/2000/svg" role="img" viewBox="0 0 24 24" fill="none" stroke="#23ce6b " stroke-width="1" stroke-linecap="round" stroke-linejoin="round" ><title>Folder</title><path d="M22 19a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h5l2 3h9a2 2 0 0 1 2 2z"></path></svg>
-              <div className="project-links">
-                <a href="https://clinic.proacademy.uz/" target="_blank" rel="noreferrer">
-                  <img src={externalLinkIcon} alt="Visitar site" />
-                </a>
-              </div>
-            </header>
-            <div className="body">
-              <a style={{ color: "#fff" }} href="https://clinic.proacademy.uz/" target="_blank" rel="noreferrer">
+        <ScrollAnimation animateIn="flipInX">
+          <a target='_blank' href='https://clinic.proacademy.uz'>
+            <div className="project">
+              <div className="body">
+                <img src={clinic} style={{width:"100%", borderRadius:"8px"}} alt="" />
                 <h3>Legal Clinic</h3>
-              </a>
-              <p>This system allows people to get legal advice for free and online. I was involved in the preparation of the front end part of this site.</p>
+                <p>This system allows people to get legal advice for free and online. I was involved in the preparation of the front end part of this site.</p>
+              </div>
+              <footer>
+              <ul className="tech-list">
+                  <li>Html</li>
+                  <li>Css</li>
+                  <li>Sass</li>
+                  <li>Bootstrap</li>
+                  <li>Antd design</li>
+                </ul>
+                <ul className="tech-list">
+                  <li>JavaScript</li>
+                  <li>React js</li>
+                  <li>Redux</li>
+                </ul>
+              </footer>
+            </div>
+          </a>
+        </ScrollAnimation>
+
+        <ScrollAnimation animateIn="flipInX">
+          <a target='_blank' href='https://bianchiwater.uz'>
+          <div className="project">
+            <div className="body">
+                <img src={bianchi} style={{width:"100%", borderRadius:"8px"}} alt="" />
+                <h3>Bianchi water</h3>
+              <p>This site is for a water filter installation company.</p>
             </div>
             <footer>
             <ul className="tech-list">
@@ -64,17 +84,63 @@ export function Portfolio() {
                 <li>Css</li>
                 <li>Sass</li>
                 <li>Bootstrap</li>
-                <li>Antd design</li>
               </ul>
               <ul className="tech-list">
                 <li>JavaScript</li>
-                <li>React js</li>
-                <li>Redux</li>
+                <li>JQuery</li>
               </ul>
             </footer>
           </div>
           </a>
-        </ScrollAnimation> */}
+        </ScrollAnimation>
+
+        <ScrollAnimation animateIn="flipInX">
+          <a target='_blank' href='https://theprocess.uz/'>
+          <div className="project">
+            <div className="body">
+              <img src={process} style={{width:"100%", borderRadius:"8px"}} alt="" />
+                <h3>Process uz</h3>
+              <p>Process LLC is a worldwide operating trading company in nuts, pulses and dried fruit and a supplier to many well known buyers.</p>
+            </div>
+            <footer>
+            <ul className="tech-list">
+                <li>Html</li>
+                <li>Css</li>
+                <li>Sass</li>
+                <li>Bootstrap</li>
+              </ul>
+              <ul className="tech-list">
+                <li>JavaScript</li>
+                <li>React js</li>
+              </ul>
+            </footer>
+          </div>
+          </a>
+        </ScrollAnimation>
+
+        <ScrollAnimation animateIn="flipInX">
+          <a target='_blank' href='https://al-arabiya.uz'>
+          <div className="project">
+            <div className="body">
+                <img src={arabiya} style={{width:"100%", borderRadius:"8px"}} alt="" />
+                <h3>Al arabiya</h3>
+              <p>This site is for an education center</p>
+            </div>
+            <footer>
+            <ul className="tech-list">
+                <li>Html</li>
+                <li>Css</li>
+                <li>Sass</li>
+                <li>Bootstrap</li>
+              </ul>
+              <ul className="tech-list">
+                <li>JavaScript</li>
+                <li>JQuery</li>
+              </ul>
+            </footer>
+          </div>
+          </a>
+        </ScrollAnimation>
 
         <ScrollAnimation animateIn="flipInX">
           <a target='_blank' href='https://tian.uz/'>
@@ -123,12 +189,12 @@ export function Portfolio() {
         </ScrollAnimation>
 
         <ScrollAnimation animateIn="flipInX">
-          <a target='_blank' href='https://theprocess.uz/'>
+          <a target='_blank' href='https://poytaxtyoshlari.uz'>
           <div className="project">
             <div className="body">
-              <img src={process} style={{width:"100%", borderRadius:"8px"}} alt="" />
-                <h3>Process uz</h3>
-              <p>Process LLC is a worldwide operating trading company in nuts, pulses and dried fruit and a supplier to many well known buyers.</p>
+                <img src={yoshlar} style={{width:"100%", borderRadius:"8px"}} alt="" />
+                <h3>Yoshlar agentligi</h3>
+              <p>This site is for "Yoshlar agentligi"</p>
             </div>
             <footer>
             <ul className="tech-list">
@@ -136,34 +202,11 @@ export function Portfolio() {
                 <li>Css</li>
                 <li>Sass</li>
                 <li>Bootstrap</li>
+                <li>Antd design</li>
               </ul>
               <ul className="tech-list">
                 <li>JavaScript</li>
                 <li>React js</li>
-              </ul>
-            </footer>
-          </div>
-          </a>
-        </ScrollAnimation>
-
-        <ScrollAnimation animateIn="flipInX">
-          <a target='_blank' href='https://bianchiwater.uz'>
-          <div className="project">
-            <div className="body">
-                <img src={bianchi} style={{width:"100%", borderRadius:"8px"}} alt="" />
-                <h3>Bianchi water</h3>
-              <p>This site is for a water filter installation company.</p>
-            </div>
-            <footer>
-            <ul className="tech-list">
-                <li>Html</li>
-                <li>Css</li>
-                <li>Sass</li>
-                <li>Bootstrap</li>
-              </ul>
-              <ul className="tech-list">
-                <li>JavaScript</li>
-                <li>JQuery</li>
               </ul>
             </footer>
           </div>
